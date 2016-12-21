89496a5ac893d42c2f329f645450565495c94bb0
Jose Carrero <jose@grupoapok.com>
2016-12-21 09:26:23 -0400

    First commit

---
+++ src/AuthClientMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Apok\Middleware;
+
+use Closure;
+
+class AuthClientMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
